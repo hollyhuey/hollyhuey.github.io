44fb1d74c76c178ffbaf40532e4d653b0176089d
Holly Huey <--global>
2023-10-15 23:20:21 -0700

    overhaul, figured out media

---
+++ js/animation.js
@@ -0,0 +1,29 @@
+// write typing text
+window.onload = function() {
+  // https://github.com/mattboldt/typed.js/
+  new Typed('#typing-text', {
+    strings: ["COMMUNICATE WITH EACH OTHER?", "LEARN ABOUT THE WORLD?", "IMPROVE OUR ENVIRONMENTS?"],
+    // strings: ["COMMUNICATE?", "LEARN?", "^500DRIVE CHANGE?"],
+    typeSpeed: 60,
+    backDelay: 1500,
+    fadeOut: false,
+    backSpeed: 30,
+    smartBackspace: true,
+    loop: true,
+    showCursor: false
+  });
+
+  // mouseover to switch headshots
+  $('#headshot').hover(function() {
+    $(this).attr('src', 'assets/photos/headshot-sketch2.png');
+  }, function() {
+    $(this).attr('src', 'assets/photos/headshot.png');
+  });
+
+};
+
+// function menuOnClick() {
+//   document.getElementById("menu-bar").classList.toggle("change");
+//   document.getElementById("hamburger").classList.toggle("change");
+//   document.getElementById("menu-bg").classList.toggle("change-bg");
+// }
